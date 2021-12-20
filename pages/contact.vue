4882d22c979c3d876faf5a1bfb953f5d591175dd
ZOYBAD14xx <60214442+ZOYBAD14xx@users.noreply.github.com>
2021-12-20 08:54:59 +0700

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -13,7 +13,7 @@
             <v-img
               class="white--text align-end"
               height="355px"
-              src="https://scontent.fvte4-1.fna.fbcdn.net/v/t1.6435-9/89964469_545224986103318_4097848638078713856_n.png?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeE4-XHqlQnwkNQhw9cQym3NhldoLV0EO7iGV2gtXQQ7uL-L9py_qJF3NRvaUjml1_MXtpKvDzyfCv2r4BQROkn-&_nc_ohc=NLlQPO6F5_EAX_K9VWc&tn=BqLYBhWQTaJ0E7J3&_nc_ht=scontent.fvte4-1.fna&oh=d81259441547a82db5388616bf15de3e&oe=6184FCD0"
+              src="https://raw.githubusercontent.com/ZOYBAD14xx/CeitSoftWareWeb/main/image/Logo.png"
             >
             </v-img>
             <v-card-title></v-card-title>
@@ -37,7 +37,7 @@
             <v-img
               class="white--text align-end"
               height="350px"
-              src="https://scontent.fvte4-1.fna.fbcdn.net/v/t31.18172-8/20157942_136501930269484_7736003330301945368_o.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeG9hMfCBMWt78xTEbfwLnwXU5BRTLYsfgdTkFFMtix-B5jN5xd9ewKQiVL0A5y6og6VM6d_JPDnI4Ycjw2ByAB-&_nc_ohc=2bI7mdr6gcAAX-4D6cW&_nc_ht=scontent.fvte4-1.fna&oh=fa5f4475aae50d68f34170b64e268b74&oe=618484C4"
+              src="https://raw.githubusercontent.com/ZOYBAD14xx/CeitSoftWareWeb/main/image/CEIT.jpg"
             >
             </v-img>
 
