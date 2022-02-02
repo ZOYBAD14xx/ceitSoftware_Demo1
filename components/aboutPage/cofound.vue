904ffabccf1c7b9b88d34deb9e3a395aff23d648
ZOYBAD14xx <60214442+ZOYBAD14xx@users.noreply.github.com>
2022-02-02 15:27:41 +0700

    Add files via upload

---
+++ components/aboutPage/cofound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <adCard :posts="getdata"/>
+  </div>
+</template>
+<script>
+export default {
+   data() {
+    return {
+      getdata: [],
+    };
+  },
+  async fetch() {
+      this.getdata = await fetch(
+       'https://raw.githubusercontent.com/ZOYBAD14xx/CeitSoftWareWeb/main/co_founderAPI.json'
+      ).then(res => res.json())
+    }
+};
+</script>
+
+<style></style>
---
+++ components/aboutPage/newMem4.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <memberCard :posts="fetchData"/>
+  </div>
+</template>
+<script>
+export default {
+   data() {
+    return {
+      getdata: [],
+      fetchData:[]
+    };
+  },
+  async fetch() {
+      this.getdata = await fetch(
+        'https://raw.githubusercontent.com/ZOYBAD14xx/CeitSoftWareWeb/main/allMember.json'
+      ).then(res => res.json())
+      this.fetchData = this.getdata[0].new_gen4;
+    }
+};
+</script>
+
+<style></style>
